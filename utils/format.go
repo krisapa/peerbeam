b9a56a5c4881c0c35d1c9141b47a6cfa7a6b6c0e
6b70 <57601958+6b70@users.noreply.github.com>
2024-09-29 17:20:18 -0400

    rename byte_format.go to more general format.go and add transfer proposal formatting

---
+++ utils/format.go
@@ -0,0 +1,34 @@
+package utils
+
+import (
+	"fmt"
+	"github.com/6b70/peerbeam/proto/compiled/controlpb"
+	"strings"
+)
+
+func ByteCountSI(b int64) string {
+	const unit = 1000
+	if b < unit {
+		return fmt.Sprintf("%d B", b)
+	}
+	div, exp := int64(unit), 0
+	for n := b / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.1f %cB",
+		float64(b)/float64(div), "kMGTPE"[exp])
+}
+
+func FormatFileProposal(fileMDList *controlpb.FileMetadataList) string {
+	var builder strings.Builder
+	for _, fileMD := range fileMDList.Files {
+		name := fileMD.FileName
+		if fileMD.IsDirectory {
+			name += "/"
+		}
+		size := ByteCountSI(fileMD.FileSize)
+		builder.WriteString(fmt.Sprintf("%s\t%s\n", name, size))
+	}
+	return builder.String()
+}
