0fab750c72f833d38d36f4cd4a9dd1966eb15b4c
6b70 <57601958+6b70@users.noreply.github.com>
2024-09-29 22:51:50 -0400

    Copy through SSH with OSC52; add recv alias

--- main.go
+++ main.go
@@ -2,17 +2,11 @@ package main
 
 import (
 	"github.com/6b70/peerbeam/cmd"
-	"golang.design/x/clipboard"
 	"log"
 )
 
 func main() {
-	err := clipboard.Init()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = cmd.App()
+	err := cmd.App()
 	if err != nil {
 		log.Fatal(err)
 	}
